b31830d4b809c8f57556c028e470dd68a216ae35
Kevyn Hale <khale@kydeveloper.com>
2018-07-29 09:40:34 -0600

    finish adding base template to unsubscribe page

--- kydeveloper/src/app/pages/subscribe/unsubscribe/unsubscribe.component.ts
+++ kydeveloper/src/app/pages/subscribe/unsubscribe/unsubscribe.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { select } from '../../../../../node_modules/@angular-redux/store';
+import { Observable } from '../../../../../node_modules/rxjs/Observable';
+import { Subscription } from '../../../../../node_modules/rxjs/Subscription';
+import { ActivatedRoute, Router } from '../../../../../node_modules/@angular/router';
+import { SubscribeActions } from '../../../../actions';
 
 @Component({
   selector: 'app-unsubscribe',
   templateUrl: './unsubscribe.component.html',
-  styleUrls: ['./unsubscribe.component.scss']
+  styleUrls: ['./unsubscribe.component.scss', './../shared-subscribe.component.scss']
 })
 export class UnsubscribeComponent implements OnInit {
 
-  constructor() { }
+  @select(['subscribe', 'unsubscribe', 'loading']) readonly loading$: Observable<boolean>
+  @select(['subscribe', 'unsubscribe', 'unsubscribed']) readonly unsubscribed$: Observable<boolean>
+  @select(['subscribe', 'unsubscribe', 'email']) readonly email$: Observable<string>
+
+  _codeParamSubscription: Subscription
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private _subscribeActions: SubscribeActions
+  ) { }
 
   ngOnInit() {
+    this._codeParamSubscription = this.route.queryParams.subscribe(
+      params => {
+        console.log(params)
+        if (params['code'] != null) {
+          this._subscribeActions.unsubscribe(params['code'])
+        } else {
+          this.router.navigate(["/subscribe"])
+        }
+      })
+  }
+
+  ngOnDestroy() {
+    this._codeParamSubscription.unsubscribe()
   }
 
 }
